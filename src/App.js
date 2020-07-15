81329d8dd7b95ae6d5824451626350ed4875722c
mpara0 <monicapara2019@gmail.com>
2020-07-15 09:51:35 -0500

    still need to add image and hyperlink on site

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import Iframe from 'react-iframe';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          GroupHub
+        </h1>
+        <p> The ultimate hub to finding your next community 🌐 </p>
         <Iframe url="https://airtable.com/embed/shrBJGllgw0q87iSC?backgroundColor=blue&viewControls=on" width="80%" height="500px"/>
       </header>
     </div>
+
+
+
   );
 }
+
 
 export default App;
