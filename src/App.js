8ca7bae4a0ae06378e7a4cab0815ae68a2e85fa3
asaxena2019 <anushkasaxena2019@gmail.com>
2020-07-15 12:52:38 -0400

    fixed footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Iframe from 'react-iframe';
 
 
-
 function App() {
   return (
     <div className="App">
@@ -15,7 +14,7 @@ function App() {
         <Iframe url="https://airtable.com/embed/shrBJGllgw0q87iSC?backgroundColor=blue&viewControls=on" width="80%" height="500px"/>
       </header>
       <footer className="App-footer">
-        <p><a href="https://www.google.com/" target="_blank">hello</a></p>
+        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">hello</a>
       </footer>
     </div>
   );
