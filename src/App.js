ccd494183230b60c08ef9071110ca1df8bbf6b64
mpara0 <monicapara2019@gmail.com>
2020-07-15 19:13:34 -0500

    added elements. just need to style them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Iframe from 'react-iframe';
-
+import world from './world.svg';
 
 function App() {
   return (
@@ -10,11 +10,17 @@ function App() {
         <h1>
           GroupHub
         </h1>
-        <p> The ultimate hub to finding your next community <span role="img" aria-label="world emoji">🌐</span></p>
+        <img src={world} className="World-logo" alt="world"/>
+        <h3> The ultimate hub to finding your next community <span role="img" aria-label="world emoji">🌐</span></h3>
+        <p> We are devoted to connecting students to different interest groups and we hope that you are able to find your community here!
+        Feel free to add your group by clicking below </p>
+        <button><a href = "https://airtable.com/shrDDDGmh2uab4oTZ">Add Group</a> </button>
+        <button> <a href = "">Join Our Slack Channel</a></button>
         <Iframe url="https://airtable.com/embed/shrBJGllgw0q87iSC?backgroundColor=blue&viewControls=on" width="80%" height="500px"/>
       </header>
       <footer className="App-footer">
-        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">hello</a>
+        <p> Made by Anushka and Monika </p>
+//<a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">hello</a>
       </footer>
     </div>
   );
