2a073ed03c6ff7fb61e60197f17c9d6bcb1c242f
asaxena2019 <anushkasaxena2019@gmail.com>
2020-07-15 22:31:52 -0400

    added formatting

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ function App() {
         <h1>
           GroupHub
         </h1>
-        <p> The ultimate hub to finding your next community <span role="img" aria-label="world emoji">🌐</span></p>
-        <button class="button">Button</button>
+        <img src={world} className="World-logo" alt="world"/>
+        <h3> The ultimate hub to finding your next community <span role="img" aria-label="world emoji">🌐</span></h3>
+        <p className="description-p"> We are devoted to connecting students to different interest groups and we hope that you are able to find your community here!
+        Feel free to add your group by clicking below </p>
+        <div>
+          <button><a href = "https://airtable.com/shrDDDGmh2uab4oTZ" target="_blank" rel="noopener noreferrer">Add Group</a> </button>
+          <button> <a href = "https://airtable.com/shrDDDGmh2uab4oTZ" target="_blank" rel="noopener noreferrer">Join Our Slack Channel</a></button>
+        </div>
         <Iframe url="https://airtable.com/embed/shrBJGllgw0q87iSC?backgroundColor=blue&viewControls=on" width="80%" height="500px"/>
       </header>
       <footer className="App-footer">
-        <p> Made by Anushka and Monika </p>
-        <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">hello</a>
+        <p className="footer-p">Made by Anushka and Monika</p>
       </footer>
     </div>
   );
